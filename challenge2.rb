a4135c210ad216625c169bfed5125c0cb3dc9b9e
thanhthedev <huythanhdao@gmail.com>
2018-08-27 15:41:39 +1000

    v2

--- challenge2.rb
+++ challenge2.rb
@@ -1,13 +1,16 @@
 puts "What would you like to order?"
 customer = gets.chomp
 backlog = [ 3, 2, 6 ]
+sold = [ 22, 6, 12 ]
+made = [ 8, 0.15, 3 ]
 if (customer == "cocktail")
     backlog[0] += 1
-    puts " The final order is #{backlog[0]} cocktails, #{backlog[1]} waters and #{backlog[2]} beers thats a total of 17 drinks"
+    puts "The final order is #{backlog[0]} cocktails, #{backlog[1]} waters and #{backlog[2]} beers"
 elsif (customer == "water")
     backlog[1] += 1
-    puts " The final order is #{backlog[0]} cocktails, #{backlog[1]} waters and #{backlog[2]} beers thats a total of 17 drinks"   
-else (customer == "beer")
+    puts "The final order is #{backlog[0]} cocktails, #{backlog[1]} waters and #{backlog[2]} beers"   
+elsif (customer == "beer")
     backlog[2] += 1
-    puts " The final order is #{backlog[0]} cocktails, #{backlog[1]} waters and #{backlog[2]} beers thats a total of 17 drinks"
+    puts "The final order is #{backlog[0]} cocktails, #{backlog[1]} waters and #{backlog[2]} beers"
+else
 end
