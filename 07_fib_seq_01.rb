56f8100df57ef9a65b21909a1e33b76219ea673e
thanhthedev <huythanhdao@gmail.com>
2018-09-06 11:54:34 +1000

    ok

--- 07_fib_seq_01.rb
+++ 07_fib_seq_01.rb
@@ -2,11 +2,12 @@
 
 # The Company has broken the keyboard on their one and only laptop, so can’t get this bit of code to run. What would the output of this code be (without running it on your own computer)? 
 
-#  def fib(num_
-#   if num < 2
-#      num
-#   else
-#      fib(num-1) + fib(num-2)
-#   end
-# end
-# puts fib(4)
+def fib(num)
+  if num < 2
+     num
+  else
+     fib(num-1) + fib(num-2)
+  end
+end
+puts fib(4)
+puts fib(3) + fib(2)
